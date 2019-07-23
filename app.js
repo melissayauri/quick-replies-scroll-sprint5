e014e765bab3669dc7a34292d2121785f1d29ece
melissa yauri <yaurimelissa@gmail.com>
2019-07-23 11:53:37 -0500

    touch

--- app.js
+++ app.js
@@ -55,3 +55,17 @@ slider.addEventListener('mousemove', (e) => {
   slider.scrollLeft = scrollLeft - walk;
   console.log(walk);
 });
+
+
+var elementoTouch= document.getElementById("areaTactil");
+//posteriormente asignamos el manejador de eventos lo cual
+// se hace de manera convencional.
+elementoTouch.addEventListener('touchstart', function(event){
+  //Comprobamos si hay varios eventos del mismo tipo
+  if (event.targetTouches.length == 1) { 
+  var touch = event.targetTouches[0]; 
+  // con esto solo se procesa UN evento touch
+  alert(" se ha producido un touchstart en las siguientes cordenas: X " + touch.pageX + " en Y " + touch.pageY);
+  }
+  
+  }, false);
